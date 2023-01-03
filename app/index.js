9ea648f6eeb2bd0b6931c900be711cc12986e357
SOAndrianov <119683674+SOAndrianov@users.noreply.github.com>
2023-01-03 19:22:46 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,23 +3,26 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const clearBtn = document.getElementById("clear-btn")
 const ulEl = document.getElementById("ul-el")
-ulEl.innerHTML = localStorage.getItem("Leads")
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 function addItem() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
-};
-inputBtn.addEventListener("click", function() {
-    addItem();
-});
+}
 inputEl.addEventListener("keypress", function(e) {
     if (e.key === 'Enter') {
         addItem();
     }
-    else {
-        
-    }
 });
+inputBtn.addEventListener("click", function() {
+    addItem();
+})
 clearBtn.addEventListener("click", function() {
     localStorage.clear()
     location.reload()
@@ -28,27 +31,13 @@ clearBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        savedItems = localStorage.getItem("Leads")
-        if (localStorage.getItem("Leads") === null) {
-            localStorage.setItem("Leads", savedItems =`
-                <li>
-                    <a target='_blank' href='${myLeads[i]}'>
-                        ${myLeads[i]}
-                    </a>
-                </li>
-            `);
-        }
-        else {
-            localStorage.setItem("Leads", listItems +=`
-                <li>
-                    <a target='_blank' href='${myLeads[i]}'>
-                        ${myLeads[i]}
-                    </a>
-                </li>
-            `);
-        }
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
-    if (localStorage.getItem("Leads") != 0){
-        ulEl.innerHTML = localStorage.getItem("Leads")
-    }
+    ulEl.innerHTML = listItems  
 }
