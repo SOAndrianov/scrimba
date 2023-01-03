932afbd9daafb578a977243849cca1ed99db2cfb
SOAndrianov <119683674+SOAndrianov@users.noreply.github.com>
2023-01-03 16:24:12 +0100

    app update

--- app/index.js
+++ app/index.js
@@ -1,24 +1,54 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const clearBtn = document.getElementById("clear-btn")
 const ulEl = document.getElementById("ul-el")
-
-inputBtn.addEventListener("click", function() {
+ulEl.innerHTML = localStorage.getItem("Leads")
+function addItem() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     renderLeads()
-})
+};
+inputBtn.addEventListener("click", function() {
+    addItem();
+});
+inputEl.addEventListener("keypress", function(e) {
+    if (e.key === 'Enter') {
+        addItem();
+    }
+    else {
+        
+    }
+});
+clearBtn.addEventListener("click", function() {
+    localStorage.clear()
+    location.reload()
+});
 
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `
+        savedItems = localStorage.getItem("Leads")
+        if (localStorage.getItem("Leads") === null) {
+            localStorage.setItem("Leads", savedItems =`
+                <li>
+                    <a target='_blank' href='${myLeads[i]}'>
+                        ${myLeads[i]}
+                    </a>
+                </li>
+            `);
+        }
+        else {
+            localStorage.setItem("Leads", listItems +=`
+                <li>
+                    <a target='_blank' href='${myLeads[i]}'>
+                        ${myLeads[i]}
+                    </a>
+                </li>
+            `);
+        }
     }
-    ulEl.innerHTML = listItems  
+    if (localStorage.getItem("Leads") != 0){
+        ulEl.innerHTML = localStorage.getItem("Leads")
+    }
 }
